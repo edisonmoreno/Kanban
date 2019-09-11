c6ffff7c32e3c6832859ddd5f0059a153874a6a1
Sorey Garcia <soreygarcia@gmail.com>
2019-09-10 20:24:03 -0500

    MasterDetail creada

--- Kanban/Kanban/Kanban/Views/MasterPage.xaml.cs
+++ Kanban/Kanban/Kanban/Views/MasterPage.xaml.cs
@@ -10,7 +10,7 @@ using Xamarin.Forms.Xaml;
 namespace Kanban.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class MasterPage : ContentPage
+    public partial class MasterPage : MasterDetailPage
     {
         public MasterPage()
         {
