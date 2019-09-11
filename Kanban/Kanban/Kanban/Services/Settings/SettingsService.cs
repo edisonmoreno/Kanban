bc3e884eaff11f556c5f15bde7cf0ac9981ef8ca
Sorey Garcia <soreygarcia@gmail.com>
2019-09-10 20:13:40 -0500

    Avance

---
+++ Kanban/Kanban/Kanban/Services/Settings/SettingsService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kanban.Services.Settings
+{
+    public class SettingsService : ISettingsService
+    {
+        public string Get(string key)
+        {
+            return null;
+        }
+
+        public void Save(string key, string value)
+        {
+            
+        }
+    }
+}
