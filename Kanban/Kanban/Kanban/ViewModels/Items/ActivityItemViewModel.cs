8142a6e591fd83ede593fbcd1a49bed3b2b44d2d
Sorey Garcia <soreygarcia@gmail.com>
2019-09-10 21:35:45 -0500

    Datos falsos en lista de actividades

---
+++ Kanban/Kanban/Kanban/ViewModels/Items/ActivityItemViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kanban.ViewModels.Items
+{
+    public class ActivityItemViewModel
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime DateTime { get; set; }
+    }
+}
